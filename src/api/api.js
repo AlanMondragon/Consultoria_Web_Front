b789aec780c26383bdd5930081e6af4ac9edabe8
AlanMondragon <20233tn075@utez.edu.mx>
2025-04-17 12:46:03 -0600

    Agrego consumo del Login

--- src/api/api.js
+++ src/api/api.js
@@ -1,2 +1,19 @@
-//Obtencion de endsPoinds del api
+import axios from 'axios';
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+console.log('API_URL:', API_URL); 
+
+export const Login = async (email, password) => {
+    try {
+        const response = await axios.post(`${API_URL}/login`, {
+            email,
+            password,
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error during login:', error);
+        throw error;
+    }
+};
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react';
+import logo from '../../img/logo.jpg';
+import { Login } from './../../api/api.js';
+import './../../styles/Home.css';
 
 
 export default function Home() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await Login(email, password); 
+      console.log('Login successful:', response);
+    } catch (error) {
+      console.error('Error during login:', error);
+    }
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  }
+
   return (
-    <div>Home</div>
-  )
+    <div className='body-login'>
+    <div className="card-login">
+      <div className="card-left">
+        <img
+          src={logo}
+          alt="Logo Consultoría JAS"
+          className="logo-img"
+        />
+      </div>
+
+      <div className="vertical-line"></div>
+
+      <div className="card-right">
+        <h2 className='h2'>Bienvenido</h2>
+        <form action="#" onSubmit={handleLogin}>
+          <input type="email" onChange={handleEmailChange} className="form-control" placeholder="Ingresa tu correo" required/>
+          <input type="password" onChange={handlePasswordChange} className="form-control" placeholder="Ingresa tu contraseña" required/>
+          
+          <div className="mb-3">
+            <a href="#" className="forgot-password">
+              ¿Olvidaste tu contraseña?
+            </a>
+          </div>
+          <button type="submit" className="btn btn-login w-100">
+            Ingresar
+          </button>
+        </form>
+      </div>
+    </div>
+    </div>
+  );
 }
