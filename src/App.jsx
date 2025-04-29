797f41742a97c8358f08963ca9f8be21963a44eb
OscarMontes <20233tn073@utez.edu.mx>
2025-04-29 08:29:40 -0600

    Termino el navbar

--- src/App.jsx
+++ src/App.jsx
@@ -78,7 +78,6 @@ function App() {
           </ProtectedRoute>
         } />
 
-        {/* Ruta de acceso denegado */}
         <Route path="/no-encontrado" element={<NoAutorizado />} />
       </Routes>
     </Router>
